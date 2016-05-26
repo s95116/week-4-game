9703126ec4415f0e293dab1d993a1c75d9152aad
Seth Keel <seth.keel@gmail.com>
2016-05-26 16:37:19 -0400

    commit 4 - javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,28 @@
 $( document ).ready(function() {
+    var numberToGuess = 53;
+    var counter = 0;
+    var numbers = [10, 5, 3, 7];
 
-var numberToGuess = 55;
-var counter = 0;
-var numbers = [10, 5, 3, 7];
-
-$('#computer-number').text(numberToGuess);
-
-
-
-
-
-
-
+    $('#computer-number').text(numberToGuess);
+    
+    for (var i=0; i< numbers.length; i++){
+      var imageCrystal = $(gems1);
+   
+      
+      imageCrystal.attr('data-num', numbers[i]);
+      
+    }
+    
+    $('#gems1').on('click', function(){
+      counter = counter + parseInt($(this).data('num'));
+      
+      $('#user-number').text(counter);
+      if (counter == numberToGuess){
+        alert('You won!!!!');
+      }else if( counter > numberToGuess){
+        alert('You lost!');
+      }
+    });
 });
+
+
